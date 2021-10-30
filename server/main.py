3734b6e2f0f7a2f5ba4a179ea1b26e8ad480cdb5
tcthinhsqt <tcthinh.sgt@gmail.com>
2021-10-30 17:30:33 +0700

    done all API

--- server/main.py
+++ server/main.py
@@ -5,6 +5,7 @@ from transformer.Transformer_model import Transformer_model
 from datetime import datetime, date
 import os
 import hashlib
+import re
 
 application = Flask(__name__)
 application.config.update(SECRET_KEY = os.urandom(24))
@@ -65,12 +66,19 @@ def result1():
     users = User.query.all()
     users = [User.getInfo(user) for user in users]
 
-    return users[1]
+    return users[2]
 
 @application.route('/api/register', methods=['POST'])
-def createUser():
+def register():
     try:
         data = request.form
+
+        idExists = User.query.filter_by(id=data['id']).first() is not None
+        emailExists = User.query.filter_by(email=data['email']).first() is not None
+
+        if(idExists == True or emailExists == True):
+            return jsonify(code = 403, message = 'Error: MSSV or Email was exists!!!')
+
         user = User(
                     id                = data['id'],
                     name              = data['name'],
@@ -78,15 +86,34 @@ def createUser():
                     gender            = data['gender'],
                     address           = data['address'],
                     email             = data['email'],
-                    password          = hashlib.md5(data['password'].encode()),
+                    password          = hashlib.md5(data['password'].encode()).hexdigest(),
                     faculty           = data['faculty'],
                     degree            = data['degree']
                     )
         db.session.add(user)
         db.session.commit()
-        return 'Register successful!!!'
+
+        return jsonify(code = 200, message = 'Register successful!!!')
     except:
         return jsonify(code = 403, message = 'Register failed!!!')
+
+@application.route('/api/login', methods=['POST'])
+def login():
+    try:
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'  #check if string is an email
+        data  = request.form
+
+        if(re.search(regex, data['username'])):
+            user = User.query.filter_by(email = data['username']).first_or_404()
+        else:
+            user = User.query.filter_by(id = data['username']).first_or_404()
+
+        if(user.password != hashlib.md5(data['password'].encode()).hexdigest()):
+            raise Exception()
+
+        return User.getInfo(user)
+    except:
+        return jsonify(code = 403, message = 'Login information does not match our records!!!')
 
 @application.route('/api/validate', methods=['POST'])
 def validate():
@@ -107,15 +134,13 @@ def validate():
 
 @application.route('/api/question', methods=['POST'])
 def question():
-    question = str(request.form.get('input_sentence')).strip()
-    if question != '':
-        try:
-            answer, question = model.evaluate(question)
-            return jsonify(answer = answer, question = question)
-        except:
-          return 'Lỗi!!!'
-    else:
-        return 'Câu hỏi không hợp lệ!!'
+    try:
+        question         = str(request.form.get('input_sentence')).strip()
+        answer, question = model.evaluate(question)
+
+        return jsonify(answer = answer, question = question)
+    except:
+        return jsonify(code = 403, message = 'The question was wrong!!!')
 
 if __name__ == "__main__":
     application.run(debug = True, host = '0.0.0.0')
