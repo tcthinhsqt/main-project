c5f64ee38d8f414eb7e25b7fc696fc8b0808ea76
dotanhaiiuh <antvvietnam@gmail.com>
2021-11-05 21:53:00 +0700

    Sua ten cua file content

--- client/src/components/elements/ShortenURLContent.vue
+++ client/src/components/elements/ShortenURLContent.vue
@@ -1,7 +1,7 @@
 <template>
     <section id="hero">
         <div class="hero-container" data-aos="fade-up">
-            <h1>QUESTION ANSWERING SYSTEM</h1>
+            <h1>HỆ THỐNG TRẢ LỜI CÂU HỎI</h1>
             <h2>A QUESTION ANSWERING SYSTEM built with powerful tools to help you research about the school.</h2>
             <a href="#asking" class="btn-get-started scrollto"><i class="bx bx-chevrons-down"></i></a>
         </div>
